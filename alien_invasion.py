7d5eabef1a28bfcf41942cc1e91ba347339aa105
Nu Oldev <01001.logos@gmail.com>
2025-04-06 12:46:02 -0600

    Refactoring code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,61 +1,60 @@
 """
-01 - Importando modulos externos (sys, pygame)
-02 - Importando modulos internos (Creados para el programa; ship, settings)
-03 - Creando la clase padre del juego.
-04 - Creacion de la ventana para los elementos del juego (surface).
-05 - Creando el loop principal del juego.
-06 - Manejo de control de eventos.
-07 - Control del frame rate del juego.
-08 - Dibujando la apariencia de la ventana en cada paso por el loop principal.
-09 - Creando instancia para uso del modulo settings.
-10 - Creando instancia para uso del modulo ship.
-11 - Dibujando la nave en la posicion correcta en cada paso por el loop.
-12 - Creando instancia de clase principal para correr el juego.
+* Refactoring
+Nota: A un método con un guión bajo antes del nombre de este, se le
+llama (helper method), estos funcionan dentro de la clase pero no están 
+destinados para el uso fuera de la función.
+01 - Creando helper method _check_events().
+02 - Creando helper method _update_screen().
+03 - Moviendo el codigo correspondiente a _check_events().
+04 - Moviendo el codigo correspondiente a _update_screen().
+05 - Simplificando el método principal run_game().
 """
 
-# 01
 import sys
 import pygame
-# 02
 from settings import Settings
 from ship import Ship
 
 
-class AlienInvasion:  # 03
+class AlienInvasion:
     """Clase general para manejar los recursos y el comportamiento del juego."""
 
     def __init__(self):
         """Iniciar el juego y crear sus recursos."""
         pygame.init()
 
-        self.clock = pygame.time.Clock()  # 07
-        self.settings = Settings()  # 09
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
         # Create a pygame window
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))  # 04
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.ship = Ship(self)  # 10
+        self.ship = Ship(self)
 
     def run_game(self):
         """Iniciar el loop principal del juego."""
 
-        while True:  # 05
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():  # 06
-                if event.type == pygame.QUIT:
-                    sys.exit()
+        while True:
+            self._check_events()  # 05
+            self._update_screen()  # 05
+            self.clock.tick(60)  # 05
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)  # 08
-            self.ship.blitme()  # 11
+    def _update_screen(self):  # 02
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)  # 04
+        self.ship.blitme()  # 04
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()  # 04
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)  # 07 - Frame rate at 60 times per second
+    def _check_events(self):  # 01
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():  # 03
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == '__main__':
-    # Make a game instance, and run the game.
-    ai = AlienInvasion()  # 12
+    ai = AlienInvasion()
     ai.run_game()
